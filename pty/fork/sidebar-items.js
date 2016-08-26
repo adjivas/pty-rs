877b6c098f7c183650f4de6713a67e3861f11a94
adjivas <adjivas@users.noreply.github.com>
2016-08-26 05:51:08 +0200

    Initial commit gh-pages

---
+++ pty/fork/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["Fork",""],["ForkError","The enum `ForkError` defines the possible errors from constructor Fork."]],"type":[["Result","The alias `Result` learns `ForkError` possibility."]]});
